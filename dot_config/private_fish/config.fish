e73b57539af1a99f071b6e304fd28ef894a85448
Robin Tully <robin@forecast.ing>
2025-04-03 12:00:01 -0700

    adding starship and lazygit
    new fish variables
    
    adding in nushell
    
    removing file

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,4 +1,9 @@
 if status is-interactive
   atuin init fish | source
+  direnv hook fish | source
+  starship init fish | source
+
+
+  alias lg "lazygit"
     # Commands to run in interactive sessions can go here
 end
